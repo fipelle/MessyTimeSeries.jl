4b4373ef504844a18f6f8669765ff724f3c90cec
fipelle <6272230+fipelle@users.noreply.github.com>
2019-10-30 16:04:50 +0000

    Small change in TSAnalysis

--- TSAnalysis/TSAnalysis.jl
+++ TSAnalysis/TSAnalysis.jl
@@ -2,18 +2,18 @@ __precompile__()
 
 module TSAnalysis
 
-	# Libraries
-	using Dates, Logging;
-	using LinearAlgebra, Distributions, Statistics;
+    # Libraries
+    using Dates, Logging;
+    using LinearAlgebra, Distributions, Statistics;
 
-	# Custom dependencies
-	const local_path = dirname(@__FILE__);
-	include("$local_path/types.jl");
-	include("$local_path/methods.jl");
-	include("$local_path/kalman.jl");
+    # Custom dependencies
+    const local_path = dirname(@__FILE__);
+    include("$local_path/types.jl");
+    include("$local_path/methods.jl");
+    include("$local_path/kalman.jl");
 
-	# Export
-	export JVector, JArray, KalmanSettings, ImmutableKalmanSettings, MutableKalmanSettings, KalmanStatus;
-	export mean_skipmissing, std_skipmissing, is_vector_in_matrix, demean, lag, companion_form, ext_companion_form;
-	export kfilter!, kforecast, ksmoother;
+    # Export
+    export JVector, JArray, KalmanSettings, ImmutableKalmanSettings, MutableKalmanSettings, KalmanStatus;
+    export mean_skipmissing, std_skipmissing, is_vector_in_matrix, demean, lag, companion_form, ext_companion_form;
+    export kfilter!, kforecast, ksmoother;
 end
