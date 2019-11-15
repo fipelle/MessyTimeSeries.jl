80ba3b76a290bb86adee65061a1bbfb423c6d5e4
fipelle <6272230+fipelle@users.noreply.github.com>
2019-11-15 01:48:30 +0000

    add: example ARIMA

---
+++ examples/arima.jl
@@ -0,0 +1,21 @@
+# Load packages
+using TSAnalysis, Optim;
+
+# Load FredData
+using FredData;
+f = Fred();
+
+# Download Industrial Production: Manufacturing (NAICS), Log-levels (monthly, NSA)
+fred_df = get_data(f, "IPGMFN", observation_start="1984-01-01", units="log");
+
+# Store data in Array{Float64,1}
+Y = fred_df.data.value;
+
+# Remove seasonality taking the YoY% transformation of the data
+Y=permutedims(Y[13:end]-Y[1:end-12]);
+
+# Settings for ARMA(1,1)
+arima_settings = ARIMASettings(Y, 0, 1, 1)
+
+# Estimate parameters
+arima_out = arima(arima_settings, NelderMead(), Optim.Options(iterations=10000, f_tol=1e-4, x_tol=1e-4, show_trace=true, show_every=500))
