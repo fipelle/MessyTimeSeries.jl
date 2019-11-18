50fa65e7c079b902a16a0768a77f7c440b179e28
fipelle <6272230+fipelle@users.noreply.github.com>
2019-11-18 23:49:49 +0000

    add: compatibility of kalman_test for multivariate problems

--- test/kalman.jl
+++ test/kalman.jl
@@ -46,8 +46,8 @@ function kalman_test(Y::JArray, B::FloatMatrix, R::SymMatrix, C::FloatMatrix, V:
         @test ksettings_input_test(ksettings5, Y, B, R, C, V);
 
         # Initial conditions
-        @test ksettings1.X0[1] == benchmark_X0;
-        @test floor.(ksettings1.P0, digits=6)[1] == benchmark_P0;
+        @test floor.(ksettings1.X0, digits=6) == benchmark_X0;
+        @test floor.(ksettings1.P0, digits=6) == benchmark_P0;
         @test ksettings1.X0 == ksettings2.X0;
         @test ksettings1.X0 == ksettings3.X0;
         @test ksettings1.X0 == ksettings4.X0;
@@ -69,17 +69,17 @@ function kalman_test(Y::JArray, B::FloatMatrix, R::SymMatrix, C::FloatMatrix, V:
             kfilter!(ksettings, kstatus);
 
             # A-priori
-            @test floor.(kstatus.X_prior[1], digits=6) == benchmark_X_prior[t];
-            @test floor.(kstatus.P_prior[1], digits=6) == benchmark_P_prior[t];
+            @test floor.(kstatus.X_prior, digits=6) == benchmark_X_prior[t];
+            @test floor.(kstatus.P_prior, digits=6) == benchmark_P_prior[t];
 
             # A-posteriori
-            @test floor.(kstatus.X_post[1], digits=6) == benchmark_X_post[t];
-            @test floor.(kstatus.P_post[1], digits=6) == benchmark_P_post[t];
+            @test floor.(kstatus.X_post, digits=6) == benchmark_X_post[t];
+            @test floor.(kstatus.P_post, digits=6) == benchmark_P_post[t];
 
             # 12-step ahead forecast
-            @test floor.(kforecast(ksettings, kstatus.X_post, 12)[end], digits=6)[1] == benchmark_X_fc[t];
-            @test floor.(kforecast(ksettings, kstatus.X_post, kstatus.P_post, 12)[1][end], digits=6)[1] == benchmark_X_fc[t];
-            @test floor.(kforecast(ksettings, kstatus.X_post, kstatus.P_post, 12)[2][end], digits=6)[1] == benchmark_P_fc[t];
+            @test floor.(kforecast(ksettings, kstatus.X_post, 12)[end], digits=6) == benchmark_X_fc[t];
+            @test floor.(kforecast(ksettings, kstatus.X_post, kstatus.P_post, 12)[1][end], digits=6) == benchmark_X_fc[t];
+            @test floor.(kforecast(ksettings, kstatus.X_post, kstatus.P_post, 12)[2][end], digits=6) == benchmark_P_fc[t];
         end
 
         # Final value of the loglikelihood
@@ -89,12 +89,12 @@ function kalman_test(Y::JArray, B::FloatMatrix, R::SymMatrix, C::FloatMatrix, V:
         X_sm, P_sm, X0_sm, P0_sm = ksmoother(ksettings, kstatus);
 
         for t=1:length(Y)
-            @test floor.(X_sm[t], digits=6)[1] == benchmark_X_sm[t];
-            @test floor.(P_sm[t], digits=6)[1] == benchmark_P_sm[t];
+            @test floor.(X_sm[t], digits=6) == benchmark_X_sm[t];
+            @test floor.(P_sm[t], digits=6) == benchmark_P_sm[t];
         end
 
-        @test floor.(X0_sm, digits=6)[1] == benchmark_X0_sm;
-        @test floor.(P0_sm, digits=6)[1] == benchmark_P0_sm;
+        @test floor.(X0_sm, digits=6) == benchmark_X0_sm;
+        @test floor.(P0_sm, digits=6) == benchmark_P0_sm;
     end
 end
 
@@ -107,22 +107,27 @@ end
     C = 0.9*ones(1,1);
     V = Symmetric(ones(1,1));
 
+    # Ones
+    ι = ones(1,1);
+
+    # Correct estimates (initial conditions)
+    benchmark_X0 = [0.0];
+    benchmark_P0 = 5.263157*ι;
+
     # Correct estimates (Kalman filter)
-    benchmark_X0 = 0.0;
-    benchmark_P0 = 5.263157;
-    benchmark_X_prior = [0.00000; 0.314999; 0.557999; 0.502199; 0.451979; 0.998999; 0.899099; 2.483999; 2.456999; 3.104999];
-    benchmark_P_prior = [5.263157; 1.0000; 1.0000; 1.8100; 2.4661; 1.0000; 1.8100; 1.0000; 1.0000; 1.0000];
-    benchmark_X_post = [0.349999; 0.619999; 0.557999; 0.502199; 1.109999; 0.998999; 2.759999; 2.729999; 3.449999; 3.659999];
-    benchmark_P_post = [0; 0; 1; 1.81; 0; 1; 0; 0; 0; 0];
-    benchmark_X_fc = [0.098850; 0.175106; 0.157595; 0.141836; 0.313496; 0.282147; 0.779505; 0.771032; 0.974381; 1.033692];
-    benchmark_P_fc = [4.843334; 4.843334; 4.923100; 4.987711; 4.843334; 4.923100; 4.843334; 4.843334; 4.843334; 4.843334];
-    benchmark_loglik = -3.358198;
+    benchmark_X_prior = [[0.00000], [0.314999], [0.557999], [0.502199], [0.451979], [0.998999], [0.899099], [2.483999], [2.456999], [3.104999]];
+    benchmark_P_prior = [5.263157*ι, ι, ι, 1.81*ι, 2.4661*ι, ι, 1.81*ι, ι, ι, ι];
+    benchmark_X_post  = [[0.349999], [0.619999], [0.557999], [0.502199], [1.109999], [0.998999], [2.759999], [2.729999], [3.449999], [3.659999]];
+    benchmark_P_post  = [0*ι, 0*ι, ι, 1.81*ι, 0*ι, ι, 0*ι, 0*ι, 0*ι, 0*ι];
+    benchmark_X_fc    = [[0.098850], [0.175106], [0.157595], [0.141836], [0.313496], [0.282147], [0.779505], [0.771032], [0.974381], [1.033692]];
+    benchmark_P_fc    = [[4.843334]*ι, [4.843334]*ι, [4.923100]*ι, [4.987711]*ι, [4.843334]*ι, [4.923100]*ι, [4.843334]*ι, [4.843334]*ι, [4.843334]*ι, [4.843334]*ι];
+    benchmark_loglik  = -3.358198;
 
     # Correct estimates (Kalman smoother)
-    benchmark_X0_sm = 0.315;
-    benchmark_P0_sm = 1.0;
-    benchmark_X_sm = [0.350000; 0.619999; 0.774129; 0.936859; 1.110000; 1.924309; 2.759999; 2.730000; 3.449999; 3.659999];
-    benchmark_P_sm = [0.0; 0.0; 0.733952; 0.733952; 0.0; 0.552486; 0.0; 0.0; 0.0; 0.0];
+    benchmark_X0_sm = [0.315];
+    benchmark_P0_sm = 1*ι;
+    benchmark_X_sm = [[0.350000], [0.619999], [0.774129], [0.936859], [1.110000], [1.924309], [2.759999], [2.730000], [3.449999], [3.659999]];
+    benchmark_P_sm = [0*ι, 0*ι, 0.733952*ι, 0.733952*ι, 0*ι, 0.552486*ι, 0*ι, 0*ι, 0*ι, 0*ι];
 
     # Benchmark data
     benchmark_data = (benchmark_X0, benchmark_P0, benchmark_X_prior, benchmark_P_prior, benchmark_X_post, benchmark_P_post, benchmark_X_fc, benchmark_P_fc, benchmark_loglik,
