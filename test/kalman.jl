192001b035d02d97a1c304ee883a147b3c4beedb
fipelle <6272230+fipelle@users.noreply.github.com>
2019-11-18 20:30:07 +0000

    split test into two functions

--- test/kalman.jl
+++ test/kalman.jl
@@ -15,25 +15,15 @@ function ksettings_input_test(ksettings::KalmanSettings, Y::JArray, B::FloatMatr
                       ksettings.store_history == store_history];
 end
 
-@testset "univariate model" begin
+"""
+    kalman_test(Y::JArray, B::FloatMatrix, R::SymMatrix, C::FloatMatrix, V::SymMatrix, benchmark_data::Tuple)
 
-    # Initialise data and state-space parameters
-    Y = [0.35 0.62 missing missing 1.11 missing 2.76 2.73 3.45 3.66];
-    B = ones(1,1);
-    R = Symmetric(1e-8*ones(1,1));
-    C = 0.9*ones(1,1);
-    V = Symmetric(ones(1,1));
+Run a series of tests to check whether the kalman.jl functions work.
+"""
+function kalman_test(Y::JArray, B::FloatMatrix, R::SymMatrix, C::FloatMatrix, V::SymMatrix, benchmark_data::Tuple)
 
-    # Correct estimates
-    benchmark_X0 = 0.0;
-    benchmark_P0 = 5.263157;
-    benchmark_X_prior = [0.00000; 0.314999; 0.557999; 0.502199; 0.451979; 0.998999; 0.899099; 2.483999; 2.456999; 3.104999];
-    benchmark_P_prior = [5.263157; 1.0000; 1.0000; 1.8100; 2.4661; 1.0000; 1.8100; 1.0000; 1.0000; 1.0000];
-    benchmark_X_post = [0.349999; 0.619999; 0.557999; 0.502199; 1.109999; 0.998999; 2.759999; 2.729999; 3.449999; 3.659999];
-    benchmark_P_post = [0; 0; 1; 1.81; 0; 1; 0; 0; 0; 0];
-    benchmark_X_fc = [0.098850; 0.175106; 0.157595; 0.141836; 0.313496; 0.282147; 0.779505; 0.771032; 0.974381; 1.033692];
-    benchmark_P_fc = [4.843334; 4.843334; 4.923100; 4.987711; 4.843334; 4.923100; 4.843334; 4.843334; 4.843334; 4.843334];
-    benchmark_loglik = -3.358198;
+    # Benchmark data
+    benchmark_X0, benchmark_P0, benchmark_X_prior, benchmark_P_prior, benchmark_X_post, benchmark_P_post, benchmark_X_fc, benchmark_P_fc, benchmark_loglik = benchmark_data;
 
     # Loop over ImmutableKalmanSettings and MutableKalmanSettings
     for ksettings_type = [ImmutableKalmanSettings; MutableKalmanSettings]
@@ -93,5 +83,34 @@ end
 
         # Final value of the loglikelihood
         @test floor.(kstatus.loglik, digits=6) == benchmark_loglik
+
+        # TODO: add kalman smoother
     end
+end
+
+@testset "univariate model" begin
+
+    # Initialise data and state-space parameters
+    Y = [0.35 0.62 missing missing 1.11 missing 2.76 2.73 3.45 3.66];
+    B = ones(1,1);
+    R = Symmetric(1e-8*ones(1,1));
+    C = 0.9*ones(1,1);
+    V = Symmetric(ones(1,1));
+
+    # Correct estimates
+    benchmark_X0 = 0.0;
+    benchmark_P0 = 5.263157;
+    benchmark_X_prior = [0.00000; 0.314999; 0.557999; 0.502199; 0.451979; 0.998999; 0.899099; 2.483999; 2.456999; 3.104999];
+    benchmark_P_prior = [5.263157; 1.0000; 1.0000; 1.8100; 2.4661; 1.0000; 1.8100; 1.0000; 1.0000; 1.0000];
+    benchmark_X_post = [0.349999; 0.619999; 0.557999; 0.502199; 1.109999; 0.998999; 2.759999; 2.729999; 3.449999; 3.659999];
+    benchmark_P_post = [0; 0; 1; 1.81; 0; 1; 0; 0; 0; 0];
+    benchmark_X_fc = [0.098850; 0.175106; 0.157595; 0.141836; 0.313496; 0.282147; 0.779505; 0.771032; 0.974381; 1.033692];
+    benchmark_P_fc = [4.843334; 4.843334; 4.923100; 4.987711; 4.843334; 4.923100; 4.843334; 4.843334; 4.843334; 4.843334];
+    benchmark_loglik = -3.358198;
+
+    # Benchmark data
+    benchmark_data = (benchmark_X0, benchmark_P0, benchmark_X_prior, benchmark_P_prior, benchmark_X_post, benchmark_P_post, benchmark_X_fc, benchmark_P_fc, benchmark_loglik);
+
+    # Run tests
+    kalman_test(Y, B, R, C, V, benchmark_data);
 end
