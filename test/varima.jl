0723602dc11195e7f6abf3ccc990f826bb5d4c1d
fipelle <6272230+fipelle@users.noreply.github.com>
2020-03-19 02:53:31 +0000

    update: tests for arima

--- test/varima.jl
+++ test/varima.jl
@@ -8,31 +8,31 @@ function varima_test(Y::Array{Float64,2}, d::Int64, p::Int64, q::Int64, benchmar
     # Benchmark data
     benchmark_X0, benchmark_P0, benchmark_B, benchmark_R, benchmark_C, benchmark_V, benchmark_fc = benchmark_data;
 
-    # Tests on ARIMASettings
-    arima_settings = ARIMASettings(Y, d, p, q);
-    @test arima_settings.d == d;
-    @test arima_settings.p == p;
-    @test arima_settings.q == q;
-    @test arima_settings.n == size(Y,1);
-    @test arima_settings.nr == size(Y,1)*max(p, q+1);
-    @test arima_settings.np == size(Y,1)*p;
-    @test arima_settings.nq == size(Y,1)*q;
-    @test arima_settings.nnp == size(Y,1)^2*p;
-    @test arima_settings.nnq == size(Y,1)^2*q;
+    # Tests on VARIMASettings
+    varima_settings = VARIMASettings(Y, d, p, q);
+    @test varima_settings.d == d;
+    @test varima_settings.p == p;
+    @test varima_settings.q == q;
+    @test varima_settings.n == size(Y,1);
+    @test varima_settings.nr == size(Y,1)*max(p, q+1);
+    @test varima_settings.np == size(Y,1)*p;
+    @test varima_settings.nq == size(Y,1)*q;
+    @test varima_settings.nnp == size(Y,1)^2*p;
+    @test varima_settings.nnq == size(Y,1)^2*q;
 
     # Estimate parameters
-    arima_out = arima(arima_settings, NelderMead(), Optim.Options(iterations=10000, f_tol=1e-2, x_tol=1e-2, g_tol=1e-2, show_trace=true, show_every=500));
+    varima_out = varima(varima_settings, NelderMead(), Optim.Options(iterations=10000, f_tol=1e-2, x_tol=1e-2, g_tol=1e-2, show_trace=true, show_every=500));
 
     # Test on the parameters
-    @test round.(arima_out.B, digits=10) == benchmark_B;
-    @test round.(arima_out.R, digits=10) == benchmark_R;
-    @test round.(arima_out.C, digits=10) == benchmark_C;
-    @test round.(arima_out.V, digits=10) == benchmark_V;
-    @test round.(arima_out.X0, digits=10) == benchmark_X0;
-    @test round.(arima_out.P0, digits=10) == benchmark_P0;
+    @test round.(varima_out.B, digits=10) == benchmark_B;
+    @test round.(varima_out.R, digits=10) == benchmark_R;
+    @test round.(varima_out.C, digits=10) == benchmark_C;
+    @test round.(varima_out.V, digits=10) == benchmark_V;
+    @test round.(varima_out.X0, digits=10) == benchmark_X0;
+    @test round.(varima_out.P0, digits=10) == benchmark_P0;
 
     # 12-step ahead forecast
-    fc = forecast(arima_out, 12, arima_settings);
+    fc = forecast(varima_out, 12, varima_settings);
     @test round.(fc, digits=10) == benchmark_fc;
 end
 
