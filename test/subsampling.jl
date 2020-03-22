ef5ef79e9cc700718ef52f6b455d942ffbcb4bde
fipelle <6272230+fipelle@users.noreply.github.com>
2020-03-22 18:18:16 +0000

    include subsampling + tests

---
+++ test/subsampling.jl
@@ -0,0 +1,17 @@
+@testset "block jackknife" begin
+
+    # Load data
+    Y = read_test_input("./input/subsampling/data");
+
+    # Settings
+    subsample = 0.2;
+
+    # Run `block jackknife` function
+    
+
+    # Load benchmark output
+    Y = read_test_input("./input/subsampling/data");
+
+    # Run tests
+
+end
