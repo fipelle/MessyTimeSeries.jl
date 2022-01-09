9d443f03326fdd99fffa6a9bd8423e1eac36e0ae
fipelle <6272230+fipelle@users.noreply.github.com>
2022-01-09 04:07:33 +0100

    Attempted fix in docs

--- src/methods.jl
+++ src/methods.jl
@@ -381,11 +381,11 @@ Interpolate each non-stationary series in `X`, in turn, using a random walk logi
 
     forward_backwards_rw_interpolation(X::FloatMatrix, n::Int64, T::Int64)
 
+Return `X`.
+
 # Arguments
 - `X`: observed measurements (`nxT`)
 - `n` and `T` are the number of series and observations
-
-Return `X`.
 """
 function forward_backwards_rw_interpolation(X::JMatrix{Float64}, n::Int64, T::Int64)
 
